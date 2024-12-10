fa9a6d69c58e1c53b86b6d39b5b2cbd2d168ccbf
Antoine Correira <antoine.correira@gmail.com>
2024-12-10 17:10:11 +0100

    commit après debug

--- routes/establishments.js
+++ routes/establishments.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const Establishment = require('../models/establishments');
 
-/* Récupérer tous les établissements*/
+/* GET home page. */
 router.get('/all', async (req, res) => {
     const establishments = await Establishment.find();
-    res.json({result: true, establishments})
+    res.json({result: true, establishments: establishments})
 });
 
 module.exports = router;
