567bc78df95e6b524930f0f22b1f013db4321478
alexis <alexis.nomine9999@gmail.com>
2024-12-19 09:16:11 +0100

    maj

--- routes/establishments.js
+++ routes/establishments.js
@@ -47,13 +47,30 @@ console.log(query)
 // Route pour récupérer toutes les villes distinctes
 router.get('/city', async (req, res) => {
   try {
-      const cities = await Establishment.distinct('city'); // Récupérer toutes les villes distinctes
-      res.json(cities); // Retourner les villes sous forme de tableau
+    const userQuery = req.query.q; // Saisie de l'utilisateur, par exemple ?q=pariss
+    if (userQuery) {
+      // Construction d'une RegEx pour chercher des correspondances proches et insensibles à la casse
+      // Utilisation de .* entre les lettres pour tolérer les fautes de frappe mais aussi une recherche plus logique
+      const regex = new RegExp(userQuery.split('').join('.*?'), 'i');
+console.log('Regex générée:', regex); // Log de la RegEx
+
+
+      // Rechercher dans la base de données avec la RegEx
+      const cities = await Establishment.distinct('city', { city: { $regex: regex } });
+
+      return res.json(cities); // Retourner les villes correspondantes
+    }
+
+    // Si aucune recherche, retourner toutes les villes distinctes
+    const allCities = await Establishment.distinct('city');
+    res.json(allCities);
   } catch (error) {
-      console.error('Erreur lors de la récupération des villes:', error);
-      res.status(500).json({ error: 'An error occurred while retrieving cities.' });
+    console.error('Erreur lors de la récupération des villes:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving cities.' });
   }
 });
+
+
 
 // Route pour récupérer tous les types d'établissement distincts
 router.get('/type', async (req, res) => {
